c993b9f6fd9b18c77402cf616cbe7302a7722d02
mohitkukreja15 <mohitkukreja802@gmail.com>
2023-09-26 06:49:04 +0530

    ice1

---
+++ ICE1connection.session.sql
@@ -0,0 +1,26 @@
+INSERT INTO movies (
+    title,
+    studio,
+    genres,
+    directors,
+    writers,
+    actors,
+    year,
+    length,
+    shortdescription,
+    mpa,
+    criticsrating
+  )
+VALUES (
+    'The Matrix',
+    'Warner Bros. Pictures',
+    'Action, sci-fi',
+    'Lana Wachowski, Lilly Wachowski',
+    'Lana Wachowski, Lilly Wachowski',
+    'Keanu Reaves,Laurence Fishburne, Carrie-Anne Moss',
+    '1999',
+    136,
+    'When a beautiful stranger leads computer hacker neo to a fordbidding underworld, he discovers the shocking truth-the life he knows is the elaborate deception of an evil cyber-intelligence.',
+    'r',
+    '8.7'
+  );
